e335ef5373d6b363d003b62a5e24443e1abd6fba
srpeteng73 <139693244+srpeteng73@users.noreply.github.com>
2025-09-13 18:03:28 -0500

    Update full3d.py

--- full3d.py
+++ full3d.py
@@ -5,26 +5,22 @@ from scipy.sparse.linalg import spsolve
 
 # --- The Grid Class ---
 class Grid:
+    # ... (This class is complete and remains unchanged)
     def __init__(self, inputs):
         grid_params = inputs.get('grid', {})
-        self.nx = grid_params.get('nx')
-        self.ny = grid_params.get('ny')
-        self.nz = grid_params.get('nz')
-        self.dx = grid_params.get('dx')
-        self.dy = grid_params.get('dy')
-        self.dz = grid_params.get('dz')
+        self.nx, self.ny, self.nz = grid_params.get('nx'), grid_params.get('ny'), grid_params.get('nz')
+        self.dx, self.dy, self.dz = grid_params.get('dx'), grid_params.get('dy'), grid_params.get('dz')
         self.num_cells = self.nx * self.ny * self.nz
-
     def get_idx(self, i, j, k):
         if 0 <= i < self.nx and 0 <= j < self.ny and 0 <= k < self.nz:
             return k * (self.nx * self.ny) + j * self.nx + i
         return -1
-
     def cell_volume_bbl(self):
         return (self.dx * self.dy * self.dz) / 5.615
 
 # --- The Rock Class ---
 class Rock:
+    # ... (This class is complete and remains unchanged)
     def __init__(self, inputs, grid):
         rock_params = inputs.get('rock', {})
         self.poro = rock_params.get('phi', np.full(grid.num_cells, 0.1)).flatten()
@@ -34,103 +30,135 @@ class Rock:
 
 # --- The Fluid Class ---
 class Fluid:
+    # ... (This class is complete and remains unchanged for now)
     def __init__(self, inputs):
         pvt_params = inputs.get('pvt', {})
         self.pb_psi = pvt_params.get('pb_psi')
         self.Bo_pb = pvt_params.get('Bo_pb_rb_stb')
         self.muo_pb = pvt_params.get('muo_pb_cp')
         self.ct_oil = pvt_params.get('ct_o_1psi', 8e-6)
-
     def Bo(self, P):
-        # A simple oil FVF model
         return self.Bo_pb * (1.0 - self.ct_oil * (P - self.pb_psi))
-    
     def mu_oil(self, P):
         return np.full_like(P, self.muo_pb)
 
 # --- The Transmissibility Class ---
 class Transmissibility:
+    # ... (This class is complete and remains unchanged)
     def __init__(self, grid, rock):
-        self.T_x = np.zeros(grid.num_cells)
-        self.T_y = np.zeros(grid.num_cells)
-        self.T_z = np.zeros(grid.num_cells)
-        conversion_factor = 0.001127
+        self.T_x = np.zeros(grid.num_cells); self.T_y = np.zeros(grid.num_cells); self.T_z = np.zeros(grid.num_cells)
+        conv = 0.001127
         for k in range(grid.nz):
             for j in range(grid.ny):
                 for i in range(grid.nx):
                     m = grid.get_idx(i, j, k)
                     if i < grid.nx - 1:
-                        m_plus_1 = grid.get_idx(i + 1, j, k)
-                        k_h = 2 * rock.kx[m] * rock.kx[m_plus_1] / (rock.kx[m] + rock.kx[m_plus_1])
-                        self.T_x[m] = conversion_factor * k_h * grid.dy * grid.dz / grid.dx
-                    # ... Y and Z calculations would go here ...
+                        mp1 = grid.get_idx(i + 1, j, k)
+                        kh = 2*rock.kx[m]*rock.kx[mp1]/(rock.kx[m]+rock.kx[mp1]); self.T_x[m] = conv*kh*grid.dy*grid.dz/grid.dx
+                    if j < grid.ny - 1:
+                        mp1 = grid.get_idx(i, j + 1, k)
+                        kh = 2*rock.ky[m]*rock.ky[mp1]/(rock.ky[m]+rock.ky[mp1]); self.T_y[m] = conv*kh*grid.dx*grid.dz/grid.dy
+                    if k < grid.nz - 1:
+                        mp1 = grid.get_idx(i, j, k + 1)
+                        kh = 2*rock.kz[m]*rock.kz[mp1]/(rock.kz[m]+rock.kz[mp1]); self.T_z[m] = conv*kh*grid.dx*grid.dy/grid.dz
 
-# --- The State Class ---
+# --- NEW: The Relative Permeability Class ---
+class RelPerm:
+    def __init__(self, inputs):
+        rp_params = inputs.get('relperm', {})
+        self.Swc = rp_params.get('Swc', 0.15)
+        self.Sor = rp_params.get('Sor', 0.25)
+        self.Sgc = 0.05 # Critical gas saturation
+        self.kro_end = rp_params.get('kro_end', 0.8)
+        self.krw_end = rp_params.get('krw_end', 0.6)
+        self.krg_end = 0.9
+        self.no = rp_params.get('no', 2.0)
+        self.nw = rp_params.get('nw', 2.0)
+        self.ng = 2.0
+
+    def calculate(self, Sw, Sg):
+        """Calculates kro, krw, krg using Corey correlations."""
+        So = 1.0 - Sw - Sg
+        # Water relative permeability
+        Swn = (Sw - self.Swc) / (1.0 - self.Swc - self.Sor)
+        krw = self.krw_end * np.power(np.clip(Swn, 0, 1), self.nw)
+        # Oil relative permeability
+        Son = (So - self.Sor) / (1.0 - self.Swc - self.Sor)
+        kro = self.kro_end * np.power(np.clip(Son, 0, 1), self.no)
+        # Gas relative permeability
+        Sgn = (Sg - self.Sgc) / (1.0 - self.Swc - self.Sgc)
+        krg = self.krg_end * np.power(np.clip(Sgn, 0, 1), self.ng)
+        return kro, krw, krg
+
+# --- UPDATED: The State Class ---
 class State:
-    def __init__(self, inputs, grid, fluid):
+    def __init__(self, inputs, grid, fluid, relperm):
         self.grid = grid
         self.fluid = fluid
+        self.relperm = relperm
         init_params = inputs.get('init', {})
+        
+        # We now have 3 primary variables per cell
         self.pressure = np.full(grid.num_cells, init_params.get('p_init_psi'))
+        self.sw = np.full(grid.num_cells, init_params.get('Sw_init'))
+        self.sg = np.zeros(grid.num_cells)
+        
         self.update_properties()
     
     def update_properties(self):
+        """Calculates all pressure- and saturation-dependent properties."""
         self.Bo = self.fluid.Bo(self.pressure)
         self.mu_oil = self.fluid.mu_oil(self.pressure)
-        self.oil_mobility = (1.0 / self.mu_oil) / self.Bo
+        # ... Add Bg, Bw, mu_gas, mu_water ...
+        
+        self.kro, self.krw, self.krg = self.relperm.calculate(self.sw, self.sg)
+        
+        self.oil_mobility = self.kro / (self.mu_oil * self.Bo)
+        # ... Add water_mobility, gas_mobility ...
 
-# --- The Well Class ---
+# --- The Well Class (Unchanged for now) ---
 class Well:
+    # ... (This class is complete for now and remains unchanged)
     def __init__(self, inputs, grid):
-        msw_params = inputs.get('msw', {})
-        schedule_params = inputs.get('schedule', {})
-        self.bhp = schedule_params.get('bhp_psi')
-        self.perforations = []
-        # For simplicity, perforate the first few blocks of the middle row
+        msw_params = inputs.get('msw', {}); schedule_params = inputs.get('schedule', {})
+        self.bhp = schedule_params.get('bhp_psi'); self.perforations = []
         for i in range(int(msw_params.get('L_ft') / grid.dx)):
             self.perforations.append(grid.get_idx(i, grid.ny // 2, grid.nz // 2))
-        
-        # Well Index (Productivity Index) - a simple model
-        self.wi = 5.0 # bbl/day/psi
+        self.wi = 5.0
 
-# --- NEW: Jacobian Assembly Function ---
-def assemble_jacobian_and_residuals(state_new, state_old, grid, rock, fluid, trans, well, dt_days):
-    num_cells = grid.num_cells
-    J = lil_matrix((num_cells, num_cells))
-    R = np.zeros(num_cells)
+# --- BLUEPRINT: Multi-Phase Jacobian and Residuals ---
+def assemble_jacobian_and_residuals(state_new, state_old, grid, rock, fluid, relperm, trans, well, dt_days):
+    num_unknowns = grid.num_cells * 3
+    J = lil_matrix((num_unknowns, num_unknowns))
+    R = np.zeros(num_unknowns)
     
-    Vp_bbl = grid.cell_volume_bbl() * rock.poro
-    
-    for m in range(num_cells):
-        # Accumulation Term
-        accum_new = Vp_bbl[m] / (state_new.Bo[m] * dt_days)
-        accum_old = Vp_bbl[m] / (state_old.Bo[m] * dt_days) * state_old.pressure[m]
-        R[m] += accum_new * state_new.pressure[m] - accum_old
-        J[m, m] += accum_new # d(Accum)/dP_m
-
-        # Flow Terms (X-direction for now)
-        # Connection to the left (m-1)
-        if m > 0 and (m % grid.nx) != 0:
-            m_minus_1 = m - 1
-            mob = state_new.oil_mobility[m_minus_1] if state_new.pressure[m_minus_1] > state_new.pressure[m] else state_new.oil_mobility[m]
-            flow = trans.T_x[m_minus_1] * mob * (state_new.pressure[m_minus_1] - state_new.pressure[m])
-            R[m] -= flow
-            J[m, m] += trans.T_x[m_minus_1] * mob # d(Flow)/dP_m
-            J[m, m_minus_1] -= trans.T_x[m_minus_1] * mob # d(Flow)/dP_m-1
-        # Connection to the right (m+1)
-        if m < num_cells - 1 and ((m + 1) % grid.nx) != 0:
-            m_plus_1 = m + 1
-            mob = state_new.oil_mobility[m] if state_new.pressure[m] > state_new.pressure[m_plus_1] else state_new.oil_mobility[m_plus_1]
-            flow = trans.T_x[m] * mob * (state_new.pressure[m_plus_1] - state_new.pressure[m])
-            R[m] -= flow
-            J[m, m] += trans.T_x[m] * mob # d(Flow)/dP_m
-            J[m, m_plus_1] -= trans.T_x[m] * mob # d(Flow)/dP_m+1
-    
-    # Well Terms
-    for perf_idx in well.perforations:
-        well_flow = well.wi * state_new.oil_mobility[perf_idx] * (state_new.pressure[perf_idx] - well.bhp)
-        R[perf_idx] += well_flow
-        J[perf_idx, perf_idx] += well.wi * state_new.oil_mobility[perf_idx]
+    # PSEUDOCODE: Loop through each cell 'm'
+    # for m in range(grid.num_cells):
+    #     # Get the indices for the 3 equations (oil, water, gas) for this cell
+    #     idx_o = m * 3
+    #     idx_w = m * 3 + 1
+    #     idx_g = m * 3 + 2
+    #
+    #     # --- Calculate Residuals (R_o, R_w, R_g) for cell 'm' ---
+    #     # R_o = Accumulation_o - Flow_o_in + Flow_o_out - Well_o
+    #     # R_w = Accumulation_w - Flow_w_in + Flow_w_out - Well_w
+    #     # R_g = Accumulation_g - Flow_g_in + Flow_g_out - Well_g
+    #
+    #     # --- Assemble 3x3 Block for the Jacobian ---
+    #     # This block contains the derivatives of the residuals in cell 'm'
+    #     # with respect to the variables in cell 'm' (P_m, Sw_m, Sg_m)
+    #     #
+    #     # J[idx_o, idx_o] = d(R_o)/d(P_m)
+    #     # J[idx_o, idx_w] = d(R_o)/d(Sw_m)
+    #     # J[idx_o, idx_g] = d(R_o)/d(Sg_m)
+    #     # ... (9 derivatives total for this block)
+    #
+    #     # --- Assemble Off-Diagonal 3x3 Blocks for Neighbors ---
+    #     # For each neighbor (e.g., m-1), calculate the derivatives of
+    #     # the residuals in cell 'm' with respect to the variables in cell 'm-1'.
+    #     #
+    #     # J[idx_o, (m-1)*3] = d(R_o)/d(P_m-1)
+    #     # ... etc.
 
     return csc_matrix(J), R
 
@@ -138,69 +166,35 @@ def simulate(inputs):
     return simulate_3D_implicit(inputs)
 
 def simulate_3D_implicit(inputs):
-    print("--- Running Phase 1b: 3D Single-Phase Implicit Engine ---")
+    print("--- Running Phase 1b: 3D Three-Phase Implicit Engine Blueprint ---")
     start_time = time.time()
-
-    # --- 1. Initialize Components ---
+    
+    # --- 1. Initialize Core Components ---
     grid = Grid(inputs)
     rock = Rock(inputs, grid)
     fluid = Fluid(inputs)
+    relperm = RelPerm(inputs)
     trans = Transmissibility(grid, rock)
     well = Well(inputs, grid)
-    
+    print("All simulation components initialized for multi-phase.")
+
     # --- 2. Initialize State ---
-    state_current = State(inputs, grid, fluid)
+    state_current = State(inputs, grid, fluid, relperm)
     
-    # --- 3. Setup Timesteps and Output Arrays ---
-    total_time_days = 30 * 365
-    num_timesteps = 360
+    # --- 3. Setup Timesteps ---
+    total_time_days = 30 * 365; num_timesteps = 360
     dt_days = total_time_days / num_timesteps
     t_days = np.linspace(0, total_time_days, num_timesteps + 1)
-    qo_stbd_list = np.zeros(num_timesteps)
-
+    
     # --- 4. Main Time-Stepping Loop ---
     for step in range(num_timesteps):
         state_old = state_current
-        state_new_guess = state_current # Start Newton iter with previous state
+        # ... (Newton-Raphson loop will go here) ...
+        pass
         
-        # --- 5. Newton-Raphson Iteration Loop ---
-        for newton_iter in range(10):
-            # Update fluid properties for the current guess
-            state_new_guess.update_properties()
-            
-            # Assemble Jacobian and Residuals
-            J, R = assemble_jacobian_and_residuals(state_new_guess, state_old, grid, rock, fluid, trans, well, dt_days)
-            
-            # Solve the linear system for the update vector
-            dx = spsolve(J, -R)
-            
-            # Update the solution
-            state_new_guess.pressure += dx
-            
-            # Check for convergence
-            if np.linalg.norm(dx) < 1e-3:
-                break
-        
-        # --- 6. End of Timestep ---
-        state_current = state_new_guess
-        
-        # Calculate and store total well rate
-        total_q = 0
-        for perf_idx in well.perforations:
-            total_q += well.wi * state_current.oil_mobility[perf_idx] * (state_current.pressure[perf_idx] - well.bhp)
-        qo_stbd_list[step] = total_q
-
-    # --- 7. Post-Process and Return Results ---
-    qo_STBpd = np.insert(qo_stbd_list, 0, 0)
-    gor = fluid.Rs(state_current.pressure[0]) # Mock GOR
-    qg_Mscfd = qo_STBpd * gor / 1000.0
-    
-    results = {
-        't_days': t_days, 
-        'qg_Mscfd': qg_Mscfd, 
-        'qo_STBpd': qo_STBpd,
-        'p3d_psi': state_current.pressure.reshape((grid.nz, grid.ny, grid.nx))
-    }
-    
-    print(f"--- 3D Implicit Engine finished in {time.time() - start_time:.2f} seconds ---")
+    # --- 5. Post-Process and Return Results (Placeholder) ---
+    qo_STBpd = 1000 * np.exp(-t_days[1:] / 500)
+    qg_Mscfd = 8000 * np.exp(-t_days[1:] / 600)
+    results = {'t_days': t_days, 'qg_Mscfd': np.insert(qg_Mscfd, 0, 0), 'qo_STBpd': np.insert(qo_STBpd, 0, 0)}
+    print(f"--- 3D Implicit Engine Blueprint finished in {time.time() - start_time:.2f} seconds ---")
     return results
